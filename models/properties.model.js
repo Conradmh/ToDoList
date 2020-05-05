b7b9290ca6396577796a5d5c0d04d34a3c1863c4
Nathan Jessup <nathanj.jessup@gmail.com>
2020-05-05 19:48:04 -0400

    Updating properties model

--- models/properties.model.js
+++ models/properties.model.js
@@ -1,7 +1,10 @@
 module.exports = (sequelize, Sequelize) => {
   const Property = sequelize.define("property", {
     name: {
-      unitNumber: Sequelize.STRING
+      type: Sequelize.STRING
+    },
+    unitNumber: {
+      type: Sequelize.STRING
     },
     houseNumber: {
       type: Sequelize.STRING
